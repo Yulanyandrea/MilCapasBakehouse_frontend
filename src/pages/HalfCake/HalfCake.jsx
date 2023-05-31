d74dea4f7ede33a5ff5cd7db3de06e020ba489b0
Yulany Munevar <andra_852@hotmail.com>
2023-05-30 19:34:26 -0500

    adding components to half cake and personal pages

--- src/pages/HalfCake/HalfCake.jsx
+++ src/pages/HalfCake/HalfCake.jsx
@@ -1,42 +1,24 @@
 import Header from '../../components/Header/Header';
-import Image from 'next/image';
-import cake from '../Cake/cake.module.css';
+import container from '../Cake/cake.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { decrementHalfCake,incrementHalfCake } from '../../feature/products/productSlice';
+import Filter from '@/components/Filter/Filter';
+import Milhojas from '@/components/Milhojas/Milhojas';
 
 const HalfCake = () => {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.products?.halfCakeAmount)
   return (
-    <>
+    <section className={container.principalContainer}>
+    <header>
       <Header />
-      <section className={cake.containerCake}>
-        <aside className={cake.containerCake__side1}>
-          <h2 className={cake.containerCake}>Sabores</h2>
-          <select name="taste" className={cake.containerCake} label="taste">
-            <option name="null"> </option>
-            <option name="taste">Arandanos</option>
-          </select>
+    </header>
 
-        </aside>
-        <aside className={cake.containerCake__side2}>
-          <Image src="/images/bocado.png" alt="" className={cake.containerCake__image} width={100} height={100}/>
-          <section className={cake.containerCake__description}>
-            <title className={cake.containerCake__title}>Arandanos</title>
-            <p className={cake.containerCake___detail}>detalles del producto</p>
-          </section>
-          <section className={cake.containerCake__amount}>
-            <button type="submit" className={cake.containerCake__subtraction} onClick={()=>dispatch(decrementHalfCake())}>-</button>
-            <label htmlFor="taste" className={cake["containerCake__amount--am"]}>{data}</label>
-            <button type="submit" className={cake.containerCake__add} onClick={()=>dispatch(incrementHalfCake())}>+</button>
-          </section>
-          <section className={cake.containerCake__submitBtn}>
-            <button type="submit" className={cake["containerCake__submitBtn--sub"]}>Agregar</button>
-          </section>
-        </aside>
-      </section>
-
-    </>
+    <aside className={container.containerCake}>
+      <h2 className={container.containerCakeTitle}>Sabores</h2>
+      <Filter />
+      <Milhojas />
+    </aside>
+  </section>
 
   );
 };
--- src/pages/Personal/Personal.jsx
+++ src/pages/Personal/Personal.jsx
@@ -1,42 +1,24 @@
 import Header from '../../components/Header/Header';
-import Image from 'next/image';
-import cake from '../Cake/cake.module.css';
+import container from '../Cake/cake.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { incrementPersonalCake,decrementPersonalCake } from '../../feature/products/productSlice';
+import Filter from '@/components/Filter/Filter';
+import Milhojas from '@/components/Milhojas/Milhojas';
 
 const Personal = () => {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.products?.personalCake)
   return (
-    <>
-      <Header />
-      <section className={cake.containerCake}>
-        <aside className={cake.containerCake__side1}>
-          <h2 className={cake.containerCake}>Sabores</h2>
-          <select name="taste" className={cake.containerCake} label="taste">
-            <option name="null"> </option>
-            <option name="taste">Arandanos</option>
-          </select>
+    <section className={container.principalContainer}>
+      <header>
+        <Header />
+      </header>
 
-        </aside>
-        <aside className={cake.containerCake__side2}>
-          <Image src="/images/personal.png" alt="" className={cake.containerCake__image} width={100} height={100}/>
-          <section className={cake.containerCake__description}>
-            <title className={cake.containerCake__title}>Arandanos</title>
-            <p className={cake.containerCake___detail}>detalles del producto</p>
-          </section>
-          <section className={cake.containerCake__amount}>
-            <button type="submit" className={cake.containerCake__subtraction} onClick={()=>dispatch(decrementPersonalCake())}>-</button>
-            <label htmlFor="taste" className={cake["containerCake__amount--am"]}>{data}</label>
-            <button type="submit" className={cake.containerCake__add} onClick={()=>dispatch(incrementPersonalCake())}>+</button>
-          </section>
-          <section className={cake.containerCake__submitBtn}>
-            <button type="submit" className={cake["containerCake__submitBtn--sub"]}>Agregar</button>
-          </section>
-        </aside>
-      </section>
-
-    </>
+      <aside className={container.containerCake}>
+        <h2 className={container.containerCakeTitle}>Sabores</h2>
+        <Filter />
+        <Milhojas />
+      </aside>
+    </section>
   );
 };
 export default Personal;
