b35c1ab58aad52e9bf5cb2be953901492712469e
Yulany Munevar <andra_852@hotmail.com>
2023-05-30 19:29:15 -0500

    align items in milhoja component

--- src/components/Milhojas/Milhojas.jsx
+++ src/components/Milhojas/Milhojas.jsx
@@ -8,21 +8,21 @@ const Milhojas = () => {
   const data = useSelector((state) => state.products?.cake);
   return (
     <>
-    <aside className={cake.containerCake__side2}>
-          <Image src="/images/bocado.png" alt="" className={cake.containerCake__image} width={100} height={100} />
-          <section className={cake.containerCake__description}>
-            <title className={cake.containerCake__title}>Arandanos</title>
-            <p className={cake.containerCake___detail}>Detalles del producto</p>
-          </section>
-          <section className={cake.containerCake__amount}>
-            <button type="submit" className={cake.containerCake__subtraction} onClick={() => dispatch(decrement())}>-</button>
-            <label htmlFor="taste" className={cake["containerCake__amount--am"]}>{data}</label>
-            <button type="submit" className={cake.containerCake__add} onClick={() => dispatch(increment())}>+</button>
-          </section>
-          <section className={cake.containerCake__submitBtn}>
-            <button type="submit" className={cake["containerCake__submitBtn--sub"]}>Agregar</button>
-          </section>
-        </aside>
+      <aside className={cake.containerCake__side2}>
+            <Image src="/images/bocado.png" alt="" className={cake.containerCake__image} width={100} height={100} />
+            <section className={cake.containerCake__description}>
+              <title className={cake.containerCake__title}>Arandanos</title>
+              <p className={cake.containerCake___detail}>Detalles del producto</p>
+            </section>
+            <section className={cake.containerCake__amount}>
+              <button type="submit" className={cake.containerCake__subtraction} onClick={() => dispatch(decrement())}>-</button>
+              <label htmlFor="taste" className={cake["containerCake__amount--am"]}>{data}</label>
+              <button type="submit" className={cake.containerCake__add} onClick={() => dispatch(increment())}>+</button>
+            </section>
+            <section className={cake.containerCake__submitBtn}>
+              <button type="submit" className={cake["containerCake__submitBtn--sub"]}>Agregar</button>
+            </section>
+      </aside>
 
     </>
   )
