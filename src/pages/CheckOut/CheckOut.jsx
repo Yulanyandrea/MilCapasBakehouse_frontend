88d3742ed7f31a9d0456b215bf735ca299a9299d
Yulany Munevar <andra_852@hotmail.com>
2023-06-13 08:38:34 -0500

    first model to check out page

---
+++ src/pages/CheckOut/CheckOut.jsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header/Header"
+import Cart from "@/components/Cart/Cart"
+
+const CheckOut = () => {
+  return (
+    <>
+      <Header />
+      <Cart />
+    </>
+  )
+}
+
+export default CheckOut
