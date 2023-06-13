09fef89bcb638d4c40cfa81284625e9340fbc2ce
Yulany Munevar <andra_852@hotmail.com>
2023-06-13 11:10:28 -0500

    adding comments into cart component due to Vercel deployment

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Cart = ({milhoja}) => {
   return(
     <section className={cart.principalCart}>
-        <Image src={milhoja?.image} alt="" className={cart.principalCart__image} width={100} height={100}/>
+        {/* <Image src={milhoja?.image} alt="" className={cart.principalCart__image} width={100} height={100}/>
         <section className={cart.principalCart__detail}>
           <h3 className={cart["principalCart__detail--title"]}>Producto</h3>
           <h3 className={cart["principalCart__detail--value"]}>{milhoja.milhojaName}</h3>
@@ -16,7 +16,7 @@ const Cart = ({milhoja}) => {
           <p className={cart["principalCart__detail--value"]}>{milhoja.price}</p>
         </section>
 
-        <button type="submit" className={cart.principalCart__button}></button>
+        <button type="submit" className={cart.principalCart__button}></button> */}
 
 
     </section>
