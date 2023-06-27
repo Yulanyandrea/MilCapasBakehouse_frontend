8dae747ec85c8ffb1a5b1254f55987640f5f9d05
Yulany Munevar <andra_852@hotmail.com>
2023-06-26 19:32:38 -0500

    creating log in form

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -6,10 +6,14 @@ const LogIn = () => {
     <section className={logIn.containerLogIn}>
         <Header />
         <section className={logIn.containerLogIn__inputs}>
-          <input type="text" />
-          <input type="text" />
-          <button>Iniciar Sesión</button>
-          <button>Registrarse</button>
+          <section className={logIn["containerLogIn__inputs--config"]}>
+            <input type="text" className={logIn["containerLogIn__inputs--configDetail"]} placeholder="Email"/>
+            <input type="text" className={logIn["containerLogIn__inputs--configDetail"]} placeholder="Password"/>
+            <button className={logIn["containerLogIn__inputs--btn"]}>Iniciar Sesión</button>
+            <button className={logIn["containerLogIn__inputs--btn"]}>Registrarse</button>
+
+          </section>
+
         </section>
 
 
