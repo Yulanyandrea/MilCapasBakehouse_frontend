a0b04d379c2c78526e2841b4d014155dbe65b222
Yulany Munevar <andra_852@hotmail.com>
2023-07-04 11:26:31 -0500

    adding modal to register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import Header from '@/components/Header/Header';
 import register from './resgister.module.css';
 
 const Register = () => {
+  const router = useRouter();
+  const [modal , setModal] = useState(false)
+
+  const handleRegisterDone = () => {
+    setModal(true)
+  }
+
+  const handleCloseModal = () => {
+    setModal(false)
+    router.push('/PrincipalPage/PrincipalPage');
+  }
   return (
     <section className={register.registerContainer}>
       <Header />
@@ -11,9 +24,19 @@ const Register = () => {
         <input type="text" className={register["registerContainer__form--input"]} placeholder="Email"/>
         <input type="text" className={register["registerContainer__form--input"]} placeholder="Dirección"/>
         <input type="text" className={register["registerContainer__form--input"]} placeholder="Contraseña"/>
-        <button className={register["registerContainer__form--btn"]}>Registrarme</button>
+        <button className={register["registerContainer__form--btn"]} onClick={handleRegisterDone}>Registrarme</button>
 
       </section>
+      {
+        modal && (
+          <div className={register.modal}>
+            <div className={register.modal__content}>
+              <p>Te has registrado exitosamente 🍰🎉 </p>
+              <button onClick={handleCloseModal}>Cerrar</button>
+            </div>
+          </div>
+        )
+      }
 
     </section>
 
