bbd61fdae4bd2505a86cc5c001c6d8b7f4daaaca
Yulany Munevar <andra_852@hotmail.com>
2023-07-13 10:43:49 -0500

    first step to validate forms

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -12,17 +12,30 @@ const Register = () => {
   // modal state
   const [modal , setModal] = useState(false)
   // validation
-  const [dataError, setDataError] = useState('*Este campo es requerido');
+  const [dataError, setDataError] = useState();
 
   const handleRegisterDone = async (e) => {
     e.preventDefault();
-
-    try {
-      const res = await createUser({ ...form });
-    } catch (error) {
-      console.error(error);
+    if(form.fullName=== undefined || form.fullName.length===0 ){
+      setDataError('* Este campo es obligatorio ')
+    }else if (form.email === undefined || form.email.length===0  ){
+      setDataError('* Este campo es obligatorio ')
+    }else if(form.address === undefined || form.address.length===0  ){
+      setDataError('* Este campo es obligatorio ')
+    }else if(form.password === undefined || form.password.length===0 ){
+      setDataError('* Este campo es obligatorio ')
     }
-    setModal(true)
+
+    else {
+      try {
+        const res = await createUser({ ...form });
+      } catch (error) {
+        console.error(error);
+      }
+      setModal(true)
+
+    }
+
   }
 
   const handleCloseModal = () => {
@@ -35,9 +48,17 @@ const Register = () => {
       <section className={register.registerContainer__form}>
         <h3>Nueva cuenta</h3>
         <input type="text" className={register["registerContainer__form--input"]} name = "fullName" placeholder="Nombre" onChange={handleChange}/>
+        {dataError ? <span className={register["registerContainer__form--error"]}>{dataError}</span> : null}
+
         <input type="text" className={register["registerContainer__form--input"]} name = "email" placeholder="Email" onChange={handleChange}/>
+        {dataError ? <span className={register["registerContainer__form--error"]}>{dataError}</span> : null}
+
         <input type="text" className={register["registerContainer__form--input"]} name = "address" placeholder="Dirección" onChange={handleChange}/>
+        {dataError ? <span className={register["registerContainer__form--error"]}>{dataError}</span> : null}
+
         <input type="password" className={register["registerContainer__form--input"]} name = "password" placeholder="Contraseña"onChange={handleChange}/>
+        {dataError ? <span className={register["registerContainer__form--error"]}>{dataError}</span> : null}
+
         <button className={register["registerContainer__form--btn"]} onClick={handleRegisterDone}>Registrarme</button>
 
       </section>
