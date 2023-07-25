a5cc7da4a7c70d34ecb780be44959fbb58067b3e
Yulany Munevar <andra_852@hotmail.com>
2023-07-24 19:24:15 -0500

    restoring folder organization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,16 @@
 import { Provider } from 'react-redux';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 import store from '../app/store';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
+    <UserProvider>
+      <Provider store={store}>
         <Component {...pageProps} />
-    </Provider>
+      </Provider>
+
+    </UserProvider>
+
   )
 }
