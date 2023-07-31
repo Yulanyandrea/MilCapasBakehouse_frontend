40361ee5556335c495772d7188f079ba084fbca0
Yulany Munevar <andra_852@hotmail.com>
2023-07-31 11:06:43 -0500

    creating new reducer for save user information logged with auth0

--- src/feature/users/userSlice.js
+++ src/feature/users/userSlice.js
@@ -16,7 +16,12 @@ export const userLogIn = createAsyncThunk('user/login', async (user)=>{
 const userReducer = createSlice({
   name: 'user',
   initialState,
-  reducers:{},
+  reducers:{
+    addUser:(state,action) => {
+      const userLog = action.payload;
+      state.userLogin.push(userLog)
+    }
+  },
   extraReducers:(builder)=>{
     builder
     .addCase(userLogIn.pending,(state)=>{
@@ -34,5 +39,5 @@ const userReducer = createSlice({
   }
 })
 
-
+export const { addUser } = userReducer.actions
 export default userReducer.reducer;
--- src/pages/Auth0Data/Auth0Data.jsx
+++ src/pages/Auth0Data/Auth0Data.jsx
@@ -1,10 +1,20 @@
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../../feature/users/userSlice';
 import auth0 from './auth0.module.css';
 import Header from '@/components/Header/Header';
 import useForm from '../../hooks/useForm';
-import { useUser } from '@auth0/nextjs-auth0/client';
+
 
 const Auth0Data = () => {
+  const dispath = useDispatch();
   const {form, handleChange} = useForm()
+
+  const handleSave = (e) => {
+    e.preventDefault()
+    dispath(addUser(form))
+
+  }
 
   return(
     <section className={auth0.containerauth0}>
@@ -14,7 +24,7 @@ const Auth0Data = () => {
         <input type="text" className={auth0["auth0Input__inputs--configDetail"]} placeholder="name" onChange={handleChange} name='name'>{useUser.name}</input>
         <input type="text" className={auth0["auth0Input__inputs--configDetail"]} placeholder="address" onChange={handleChange} name='address'/>
         <input type="text" className={auth0["auth0Input__inputs--configDetail"]} placeholder="phone" onChange={handleChange} name='phone'/>
-        <button className={auth0["auth0Input__inputs--btn"]} >Guardar</button>
+        <button className={auth0["auth0Input__inputs--btn"]} onClick={handleSave}>Guardar</button>
 
       </section>
 
