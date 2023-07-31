d63d498594973018041c73a5a49f9c18cea3d21e
Yulany Munevar <andra_852@hotmail.com>
2023-07-31 10:44:40 -0500

    adding auth0 page information

--- src/pages/Auth0Data/Auth0Data.jsx
+++ src/pages/Auth0Data/Auth0Data.jsx
@@ -1,6 +1,7 @@
 import auth0 from './auth0.module.css';
 import Header from '@/components/Header/Header';
 import useForm from '../../hooks/useForm';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 const Auth0Data = () => {
   const {form, handleChange} = useForm()
@@ -9,8 +10,8 @@ const Auth0Data = () => {
     <section className={auth0.containerauth0}>
       <Header />
       <section className={auth0.auth0Input}>
-        <input type="text" className={auth0["auth0Input__inputs--configDetail"]} placeholder="Email" onChange={handleChange} name='email'/>
-        <input type="text" className={auth0["auth0Input__inputs--configDetail"]} placeholder="name" onChange={handleChange} name='name'/>
+        <input type="text" className={auth0["auth0Input__inputs--configDetail"]} placeholder="Email" onChange={handleChange} name='email'>{useUser.email} </input>
+        <input type="text" className={auth0["auth0Input__inputs--configDetail"]} placeholder="name" onChange={handleChange} name='name'>{useUser.name}</input>
         <input type="text" className={auth0["auth0Input__inputs--configDetail"]} placeholder="address" onChange={handleChange} name='address'/>
         <input type="text" className={auth0["auth0Input__inputs--configDetail"]} placeholder="phone" onChange={handleChange} name='phone'/>
         <button className={auth0["auth0Input__inputs--btn"]} >Guardar</button>
