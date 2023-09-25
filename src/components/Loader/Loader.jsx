eb0bf1f13226c7fe27f1e12bbb9aaf77c6db4859
Yulany Munevar <ddb_validator@apple.com>
2023-09-25 15:46:14 -0500

    loader library ready to use

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,2 +1,19 @@
 import Loader from "react-loader-spinner";
 
+const Load = () => {
+    return (
+        <>
+            <Loader
+                type="Puff"
+                color="#572b0c"
+                height={100}
+                width={100}
+                timeout={3000} 
+            /> 
+        
+        </>
+
+    )
+}
+
+export default Load ;
