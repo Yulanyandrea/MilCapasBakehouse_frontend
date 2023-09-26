bdb0ca637e29c1d3c88fb3632c82f7d8d02fe9d4
Yulany Munevar <ddb_validator@apple.com>
2023-09-25 19:43:45 -0500

    loader component implemented in registration page

---
+++ src/components/Load/Load.jsx
@@ -0,0 +1,15 @@
+import loader from './loader.module.css';
+
+const Load = () => {
+    return (
+     
+         <section className={loader.Container}>
+            <div className={loader.loader}></div>
+
+         </section>
+     
+
+    )
+}
+
+export default Load ;
